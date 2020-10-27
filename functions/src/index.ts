3b1c8396da495d067d6eb2f1ed95bba8e70d4e43
bmstyojaop <bmstyojaop@gmail.com>
2020-10-28 01:53:22 +0900

    create chat-bot

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,27 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
+admin.initializeApp(); //adminを初期化
+const db = admin.firestore(); //admin.firestoreを定数に入れておく
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//レスポンスを返すための関数
+const sendResponse = (response: functions.Response, statusCode: number, body: any) => {
+  response.send({
+    statusCode,
+    body: JSON.stringify(body)
+  })
+}
+
+// APIの中身
+export const addDataset = functions.https.onRequest(async(req: any, res: any) => {
+　if (req.method != 'POST') {
+  // リクエストがPOSTではない時の処理
+  sendResponse(res, 405, {error: 'Invalid Request!'})
+  } else {
+    const dataset = req.body
+    for (const key of Object.keys(dataset)) {
+      const data = dataset[key]
+      await db.collection('questions').doc(key).set(data)
+    }
+    sendResponse(res, 200, {message: 'Successfully added dataset!'})
+  }
+})
---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC1kFUo2VCk2_RFxO_t3PuPkvqcBARWOgA",
+  authDomain: "takahashikota.firebaseapp.com",
+  databaseURL: "https://takahashikota.firebaseio.com",
+  projectId: "takahashikota",
+  storageBucket: "takahashikota.appspot.com",
+  messagingSenderId: "240275371679",
+  appId: "1:240275371679:web:9acc5012dafe4cc42bd4aa",
+  measurementId: "G-G7D89NLKBD"
+};
+
+export default firebaseConfig
